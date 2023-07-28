4f53defbc29660d44c116246bb02fc506db927fa
pySoo <soopy368@gmail.com>
2023-07-28 20:37:56 +0900

    [feat] : gpt api 사용에 필요한 상수들 분리

---
+++ src/constants/api.js
@@ -0,0 +1,5 @@
+export const END_POINT = {
+  CODE_GENERATION: '/api/code-generation',
+};
+
+export const OPENAI_COMPLETIONS = 'https://api.openai.com/v1/completions';
---
+++ src/constants/index.js
@@ -0,0 +1,3 @@
+export * from './api.js';
+export * from './error.js';
+export * from './gpt.js';
